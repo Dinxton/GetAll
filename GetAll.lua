60c8c556589a38120d89b0b2e4a12bfa19ea9ffe
Dinxton <dinxton@gmail.com>
2018-11-15 18:32:20 +0000

    Added GetAll.lua

---
+++ GetAll.lua
@@ -0,0 +1,12 @@
+os.loadAPI("dc.lua")
+
+shell.run("github", "clone", "dinxton/dcore", "/")
+print("Finished executing.")
+
+print("Press Any Key To Continue...")
+input = read()
+if input == "1" then
+  dc.cExit()
+else
+  dc.cExit()
+end
