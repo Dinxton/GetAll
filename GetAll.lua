18b6e720c61bd6edc2717cb1d37b98e881b2ac67
Dinxton <dinxton@gmail.com>
2018-11-16 09:53:30 +0000

    Updated GetAll.lua

--- GetAll.lua
+++ GetAll.lua
@@ -1,12 +1,45 @@
-os.loadAPI("dc.lua")
+--GetAll - A bespoke frontend for eric-wieser's computercraft-github cloner.
+--Depends on: DCore (dc/lua) version 0.0.1; Latest computercraft-github
 
-shell.run("github", "clone", "dinxton/dcore", "/")
-print("Finished executing.")
+--Imports
+os.loadAPI("dc")
 
-print("Press Any Key To Continue...")
-input = read()
-if input == "1" then
-  dc.cExit()
-else
-  dc.cExit()
+--List of programs
+DC = "dinxton/dcore"
+GA = "dinxton/GetAll"
+
+--Initialising variables
+counter = 0
+--Functions
+function fGithub(link)
+  shell.run("github", "clone", link, "/")
 end
+
+
+function downloadNotify (selection)
+  term.write("Downloading selection "..selection)
+  dc.newLine()
+  fGithub(selection)
+end
+
+function commandsInput
+  if counter = 0 then
+    term.write("Please enter the number of the program you wish to download.")
+  else
+    term.write("Do you want to download another program?")
+  end
+  dc.newLine()
+  term.write("DC - DCore - dc.lua")
+  dc.newLine()
+  term.write("GA - GetAll - GetAll.lua")
+  dc.newLine()
+  local input = read()
+  downloadNotify(input)
+  counter+=1
+end
+--Clearing screen
+dc.clearScrn()
+--Program selection
+
+
+commandsInput()
